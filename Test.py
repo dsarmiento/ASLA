560765e3022175cc1e570206aec66b3469302162
Daniel Sarmiento <d.sarmiento731@gmail.com>
2016-08-10 21:27:58 -0400

    Project setup

---
+++ Test.py
@@ -0,0 +1,8 @@
+import sys
+print "Python Version: ", sys.version
+import scipy
+print "SciPy Version: ", scipy.__version__
+import numpy
+print "NumPy Version: ", numpy.__version__
+import sklearn
+print "SkLearn Version: ", sklearn.__version__
