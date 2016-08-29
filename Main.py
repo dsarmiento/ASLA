c4dd2819573b0ef3449d91b2e32d8bedd510b126
Daniel Sarmiento <d.sarmiento731@gmail.com>
2016-08-29 00:34:32 -0400

    Added a 3s buffer. Rough detection of movement needs testing.

--- Main.py
+++ Main.py
@@ -1,30 +1,53 @@
 import serial
 import sys
 import glob
-from datetime import datetime
+import time
 
-BluetoothName = "Test"
+BLUETOOTH_NAME = "Test"
+DATA_RATE = 100
+SIGN_LENGTH = 3
+SAMPLE_LENGTH = 20
+SLOPE_THRESHOLD = 50
 
 def main():
+    # Open the right COM port
     com = None
     results = serial_ports()
     for port, name in results:
-        if name == BluetoothName:
+        if name == BLUETOOTH_NAME:
             com = serial.Serial(port)
     if com is None:
         print 'No bluetooth found'
         return
 
     # Start logging
-    filename = str(datetime.now().strftime('%m-%d-%Y %I%M%p'))
+    filename = str(time.strftime('%m-%d-%Y %I%M%p'))
     filename += '.csv'
     log = open(filename, 'w')
     flag = True
+    dataBuf = []
     while flag:
         if com.in_waiting > 0:
-            buf = com.read_all()
-            log.write(buf)
+            temp = com.read_until()
+            temp = temp.split(',')
+            dataBuf.append(temp)
+            if dataBuf.__sizeof__() > DATA_RATE * SIGN_LENGTH:
+                dataBuf = dataBuf[1:]
+
+            movement(dataBuf)
+            log.write(temp)
+
+    # Close everything we opened
+    com.close()
+    log.close()
     return
+
+def movement(data):
+    for i in range(0, DATA_RATE):
+        slope = float(data[i+SAMPLE_LENGTH] - data[i]) / float(SAMPLE_LENGTH)
+        if abs(slope) > SLOPE_THRESHOLD:
+            return True
+    return False
 
 
 # List all connected serial ports and names
