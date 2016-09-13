365b8c374a56b83a330bc72b34317a68faa5961b
Daniel Sarmiento <d.sarmiento731@gmail.com>
2016-09-13 16:42:12 -0400

    Playing friendly with the arduino IDE

---
+++ Prototype/Prototype.ino
@@ -0,0 +1,56 @@
+#include "SparkFunLSM6DS3.h"
+#include "Wire.h"
+#include "SPI.h"
+
+LSM6DS3 SensorOne( I2C_MODE, 0x6A );
+LSM6DS3 SensorTwo( I2C_MODE, 0x6B );
+bool testing = false;
+
+void setup()
+{
+    Serial.begin(9600);
+    delay(1000);
+    if(SensorOne.begin() != 0)
+    {
+        Serial.println("Problem starting sensor 0x6A");
+    }
+    if(SensorTwo.begin() != 0)
+    {
+        Serial.println("Problem starting sensor 0x6B");
+    }
+}
+
+void loop()
+{
+    String data = "";
+    long start = millis();
+    
+    data += String(analogRead(0)); data += ",";
+    data += String(analogRead(1)); data += ",";
+    data += String(analogRead(2)); data += ",";
+    data += String(analogRead(3)); data += ",";
+    data += String(SensorOne.readFloatAccelX()); data += ",";
+    data += String(SensorOne.readFloatAccelY()); data += ",";
+    data += String(SensorOne.readFloatAccelZ()); data += ",";
+    data += String(SensorOne.readFloatGyroX()); data += ",";
+    data += String(SensorOne.readFloatGyroY()); data += ",";
+    data += String(SensorOne.readFloatGyroZ()); data += ",";
+    data += String(SensorTwo.readFloatAccelX()); data += ",";
+    data += String(SensorTwo.readFloatAccelY()); data += ",";
+    data += String(SensorTwo.readFloatAccelZ()); data += ",";
+    data += String(SensorTwo.readFloatGyroX()); data += ",";
+    data += String(SensorTwo.readFloatGyroY()); data += ",";
+    data += String(SensorTwo.readFloatGyroZ());
+    
+    long end = millis();
+    
+    if(testing)
+    {
+        Serial.println(end-start);
+    }
+    else
+    {
+        Serial.println(data);
+    }
+
+}
