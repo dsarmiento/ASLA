e847ee71a23ef63989959669d48aa71fceda4656
Daniel Sarmiento <d.sarmiento731@gmail.com>
2016-09-17 22:49:03 -0400

    Added menu

--- Main.py
+++ Main.py
@@ -10,41 +10,48 @@ SAMPLE_LENGTH = 20
 SLOPE_THRESHOLD = 50
 
 def main():
-    # Open the right COM port
-    com = None
-    results = serial_ports()
-    for port in results:
-        if port == BLUETOOTH_NAME:
-            com = serial.Serial(port)
-    if com is None:
-        print 'No bluetooth found'
-        return
+    print "1. Connect"
+    print "2. Translate"
+    print "3. Learn/Log"
+    print "4. Quit"
+    choice = input("Choose an option: ")
+    print
 
-    # Start logging
-    filename = str(time.strftime('%m-%d-%Y %I%M%p'))
-    filename += '.csv'
-    log = open(filename, 'w')
-    flag = True
-    dataBuf = []
-    cnt = 0
-    while flag:
-        if com.in_waiting > 0:
-            temp = com.read_until()
-            cnt += 1
-            log.write(temp)
-            temp = temp.split(',')
-            dataBuf.append(temp)
-            if dataBuf.__sizeof__() > DATA_RATE * SIGN_LENGTH:
-                dataBuf = dataBuf[1:]
-            if cnt > 999 :
-                flag = False
-            #movement(dataBuf)
+    if choice == 1:
+        com = connect()
+    if choice == 2:
+        print "Not supported"
+    if choice == 3:
+        print "Logging started"
 
+        # Start logging
+        filename = str(time.strftime('%m-%d-%Y %I%M%p'))
+        filename += '.csv'
+        print filename
 
-    # Close everything we opened
-    com.close()
-    log.close()
-    return
+        log = open(filename, 'w')
+        flag = True
+        dataBuf = []
+        cnt = 0
+        while flag:
+            if com.in_waiting > 0:
+                temp = com.read_until()
+                cnt += 1
+                log.write(temp)
+                temp = temp.split(',')
+                dataBuf.append(temp)
+                if dataBuf.__sizeof__() > DATA_RATE * SIGN_LENGTH:
+                    dataBuf = dataBuf[1:]
+                if cnt > 999 :
+                    flag = False
+
+        # Close everything we opened
+        com.close()
+        log.close()
+
+    if choice == 4:
+        return False
+    return True
 
 def movement(data):
     for i in range(0, DATA_RATE):
@@ -75,6 +82,21 @@ def serial_ports():
             pass
     return result
 
+def connect():
+    # Open the right COM port
+    com = None
+    results = serial_ports()
+    for port in results:
+        if port == BLUETOOTH_NAME:
+            com = serial.Serial(port)
+            return com
+    if com is None:
+        print 'No bluetooth found'
+        return 0
+
 
 if __name__ == '__main__':
-    main()
+    flag = True
+    while flag:
+        flag = main()
+        print
